4edbffad5c0833de76abab98e656b1dc0c6667a6
Spadalow <abdullatif.skejic@gmail.com>
2021-03-11 00:29:57 +0100

    Sum

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+var numOne= numOne;
+var numTwo = numTwo ;
+var sum = numOne + numTwo;
+    // Only change code above this line
+    return sum;
+
+}
+
+console.log(main(15, 8)); 
+console.log(main(5, 28));//change this line
+console.log(main(150, 425));
+
+module.exports = main;
